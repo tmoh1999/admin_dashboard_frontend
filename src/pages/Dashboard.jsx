9dd255fd48101a42c6d724eb895951c4a5fd5e47
mohamed <mohamedtouabti1999@gmail.com>
2026-07-02 22:58:56 +0100

    app: routes and dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+export default function Dashboard() {
+  return (
+  <div className="flex flex-col bg-slate-300 items-center p-2 sm:p-6 min-w-150  w-full sm:w-full ">
+      <h1 className="text-3xl w-full  font-bold text-center mb-4">
+        Dashboard
+      </h1>
+ </div>
+  );
+}
