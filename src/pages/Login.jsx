1a9c18ff7ef420a564c3f9f403bc490f1d9c5929
mohamed <mohamedtouabti1999@gmail.com>
2026-07-03 00:07:39 +0100

    fix: login page style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,14 +44,15 @@ export default function Login() {
         {error && (
           <div className="bg-red-100 text-red-700 p-2 rounded">{error}</div>
         )}
-        
-        <label htmlFor="login">Login</label>      
-        <input onChange={handleChange} placeholder="Username or Email Address" value={formData.login} id="login" name="login" className="border p-2 rounded-lg w-full"  required/>
-
-        <label htmlFor="password">Password</label>
-        <input onChange={handleChange} placeholder="Password" value={formData.password} id="password" type="password" className="border p-2 rounded-lg w-full"   name="password" required/>
-
-        <button type="submit" disabled={loading} className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-700">{loading ? "Logging in..." : "Login"}</button>
+        <div className="w-full">
+          <label htmlFor="login" className="block text-xl  text-gray-700 font-medium mb-1">Login</label>      
+          <input onChange={handleChange} placeholder="Username or Email Address" value={formData.login} id="login" name="login" className="border p-2 rounded-lg w-full"  required/>
+        </div>
+        <div className="w-full">
+          <label htmlFor="password" className="block text-xl  text-gray-700 font-medium mb-1">Password</label>
+          <input onChange={handleChange} placeholder="Password" value={formData.password} id="password" type="password" className="border p-2 rounded-lg w-full"   name="password" required/>
+        </div>
+        <button type="submit" disabled={loading} className="bg-blue-600 text-white py-2 px-8 font-medium text-xl rounded-xl  hover:bg-blue-700">{loading ? "Logging in..." : "Login"}</button>
         <p>No account? 
           <Link to="/register" className="ml-2 text-green-600 font-medium hover:underline">
               Register
