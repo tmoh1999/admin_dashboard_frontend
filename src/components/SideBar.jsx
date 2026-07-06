85e7bb93fdd2a23a9e87e61202bba65e4d2d8700
mohamed <mohamedtouabti1999@gmail.com>
2026-07-06 18:17:09 +0100

    comp: sidebar component

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,50 @@
+import { NavLink ,useNavigate} from "react-router-dom";
+import { LayoutDashboard , Package, ShoppingCart, Receipt, Menu ,LogOut,
+  PackageOpen ,UserRoundPen,ContactRound,RotateCcw } from "lucide-react";
+import { useState } from "react";
+export default function Sidebar() {
+  const [open, setOpen] = useState(true);
+  return (
+    
+    <div className={`h-screen flex flex-col bg-gray-900 text-white p-4 transition-all duration-300
+      ${open ? "w-64 fixed left-0 top-0 z-50" : "w-20 relative"}`}>
+      
+      {/* Top */}
+      <div className="flex items-center justify-between mb-6">
+      {open &&
+        <h1 className={`text-2xl font-bold`}>
+          DeltaPOS 
+        </h1>
+       }
+        <button onClick={() => setOpen(!open)}>
+          <Menu size={28} className="text-white" />
+        </button>
+      </div>
+
+      {/* Links */}
+      <div className="flex-1 overflow-y-auto no-scrollbar   gap-3">
+       <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            `flex items-center gap-2 p-3 rounded-lg transition
+            ${isActive ? "bg-gray-700" : "hover:bg-gray-800"}`
+          }
+        >
+          <LayoutDashboard size={22} />
+          {open && <span>Dashboard</span>}
+        </NavLink>
+
+        <NavLink
+            to="/logout"
+            className={({ isActive }) =>
+                `flex items-center gap-2 p-3 rounded-lg transition
+                ${isActive ? "bg-gray-700" : "hover:bg-gray-800"}`
+            }
+            >
+            <LogOut size={22} />
+            {open && <span>LogOut</span>}
+        </NavLink>
+      </div>
+    </div>
+  );
+}
