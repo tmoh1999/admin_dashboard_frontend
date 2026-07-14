31f91404e79c03cd843f81891726dfd334333c50
mohamed <mohamedtouabti1999@gmail.com>
2026-07-14 12:02:20 +0100

    api: table_data: add/update/delete users requests

--- src/api/table_data.js
+++ src/api/table_data.js
@@ -1,8 +1,9 @@
+import { request } from "./http";
 export const api = {
     users: {
-        add: null,
-        update: null,
-        remove: null,
+        add: addUser,
+        update: updateUser,
+        remove: removeUser,
         profilePath:"/user/profile",
         profileKeys:["id"],
         rootpath:"/api/users",
@@ -11,8 +12,31 @@ export const api = {
 export const columns= {
     users:[
         { label: "ID", accessor: "id",edit:false },
-        { label: "Username", accessor: "username",edit:false },
-        { label: "Email", accessor: "email",edit:false },
-        { label: "Role", accessor: "role",edit:false },
+        { label: "Username", accessor: "username",edit:true },
+        { label: "Email", accessor: "email",edit:true },
+        { label: "IsEmailVerified", accessor: "is_email_verified",edit:false },
+        { label: "Role", accessor: "role",edit:true },
+        { label: "IsActive", accessor: "is_active",edit:true },
+        { label: "Created At", accessor: "created_at",edit:false },
     ],
 };
+
+export function updateUser(rowData) {
+  const  path=api.users.rootpath+"/"+rowData.id;  
+  return request(path, {
+    method: "PUT",
+    body: JSON.stringify(rowData),
+  });
+}
+
+export function addUser(rowData) {
+  return request(api.users.rootpath, {
+    method: "POST",
+    body: JSON.stringify(rowData),
+  });
+}
+export function removeUser(path) {
+  return request(path, {
+    method: "DELETE",
+  });
+}
